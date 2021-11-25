377130b3ac096981e32b5e1b6d86f3eb5c647bca
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-11-25 10:22:23 +0300

    Update Program.cs

--- C_Sharp_basics/Program.cs
+++ C_Sharp_basics/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+try
+{
+    int a = 33;
+    int b = 600;
+    byte c = checked((byte)(a + b)); // checked - выбрасывает исключение в случае переполнения указанного типа
+    Console.WriteLine(c);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine(ex.Message);
+}
