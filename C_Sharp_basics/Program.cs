1b42abbdd43b22a5f8cd3cfcca3a74ce746861ce
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-12-09 15:40:52 +0300

    Method Overloadind example

--- C_Sharp_basics/Program.cs
+++ C_Sharp_basics/Program.cs
@@ -1,18 +1,38 @@
-﻿Person vasya = new();
-
-string myName = vasya.Name;
-Console.WriteLine(myName);
-
-vasya.Name = "Vasya!";
-myName = vasya.Name;
-Console.WriteLine(myName);
-
-class Person
+﻿class Calculator
 {
-    private string name = "Undefined";
-    public string Name 
-    { 
-        get { return name; } 
-        set { name = value; } 
+    public void Add(int a, int b)
+    {
+        int result = a + b;
+        Console.WriteLine($"Result is {result}");
+    }
+    public void Add(int a, int b, int c)
+    {
+        int result = a + b + c;
+        Console.WriteLine($"Result is {result}");
+    }
+    public int Add(int a, int b, int c, int d)
+    {
+        int result = a + b + c + d;
+        Console.WriteLine($"Result is {result}");
+        return result;
+    }
+    public void Add(double a, double b)
+    {
+        double result = a + b;
+        Console.WriteLine($"Result is {result}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        calc.Add(1, 2); // 3
+        calc.Add(1, 2, 3); // 6
+        calc.Add(1, 2, 3, 4); // 10
+        calc.Add(1.4, 2.5); // 3.9
+
+        Console.ReadKey();
     }
 }
