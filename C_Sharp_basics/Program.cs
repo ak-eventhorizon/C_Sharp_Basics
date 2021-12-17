6972cc3cdb8b11a2f64956ac59070acb2f2ad8b0
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-12-17 14:52:10 +0300

    Update Program.cs
    interface example

--- C_Sharp_basics/Program.cs
+++ C_Sharp_basics/Program.cs
@@ -1,20 +1,28 @@
-﻿List<User> users = new List<User>()
-{
-    new User { Name = "Tom", Age = 33 },
-    new User { Name = "Bob", Age = 30 },
-    new User { Name = "Tom", Age = 21 },
-    new User { Name = "Sam", Age = 43 }
-};
- 
-var sortedUsers = from u in users
-                  orderby u.Age descending
-                  select u;
- 
-foreach (User u in sortedUsers)
-    Console.WriteLine(u.Name + " " + u.Age);
+﻿User vasya = new User("Vasya");
+vasya.Speak("Hello everyone!");
+vasya.Jump();
 
 
-class User {
-    public string? Name{get; set;}
-    public int Age{get; set;}
+interface IHuman {
+    void Jump();
+    void Speak(string message);
 }
+
+
+
+class User : IHuman {
+
+    string Name;
+    public User(string name){
+        Name = name;
+    }
+
+    public void Jump(){
+        Console.WriteLine(this.Name + " подпрыгнул!");
+    }
+
+    public void Speak(string msg){
+        Console.WriteLine($"{this.Name} говорит: \"{msg}\"");
+    }
+}
+
