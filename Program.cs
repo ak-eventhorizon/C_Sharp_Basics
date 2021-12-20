39c0af92c725378eb16e272616c8283cb64383b0
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-12-20 14:55:01 +0300

    Project structure split by files

--- Program.cs
+++ Program.cs
@@ -8,32 +8,12 @@ namespace C_Sharp_Basics {
 
         // метод Main() - единственный на проект, точка входа в исполнение программы
         static void Main(string[] args) {
-            User vasya = new User("Vasya");
-            vasya.Speak("Hello everyone!");
-            vasya.Jump();
+            // User vasya = new User("Vasya");
+            // vasya.Speak("Hello everyone!");
+            // vasya.Jump();
+
+            
+
         }
     }    
-
-
-    interface IHuman {
-        void Jump();
-        void Speak(string message);
-    }
-
-
-    class User : IHuman {
-
-        string Name;
-        public User(string name) {
-            Name = name;
-        }
-
-        public void Jump() {
-            Console.WriteLine(this.Name + " подпрыгнул!");
-        }
-
-        public void Speak(string msg) {
-            Console.WriteLine($"{this.Name} говорит: \"{msg}\"");
-        }
-    }
 }
---
+++ User.cs
@@ -0,0 +1,29 @@
+using System;
+
+// пространство имен - по имени проекта
+namespace C_Sharp_Basics {
+
+    interface IHuman {
+        void Jump();
+        void Speak(string message);
+    }
+
+
+    class User : IHuman {
+
+        string Name;
+        
+        // Конструктор
+        public User(string name) {
+            Name = name;
+        }
+
+        public void Jump() {
+            Console.WriteLine(this.Name + " подпрыгнул!");
+        }
+
+        public void Speak(string msg) {
+            Console.WriteLine($"{this.Name} говорит: \"{msg}\"");
+        }
+    }
+}
