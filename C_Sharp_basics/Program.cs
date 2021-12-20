94019ba0751e2b34440a55679c67877f8cd40802
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-12-20 10:52:20 +0300

    Update Program.cs
    Basic project structure example

--- C_Sharp_basics/Program.cs
+++ C_Sharp_basics/Program.cs
@@ -1,28 +1,37 @@
-﻿User vasya = new User("Vasya");
-vasya.Speak("Hello everyone!");
-vasya.Jump();
+﻿using System;
 
 
-interface IHuman {
-    void Jump();
-    void Speak(string message);
+namespace C_Sharp_Basics {
+
+    class Program {
+
+        static void Main(string[] args) {
+            User vasya = new User("Vasya");
+            vasya.Speak("Hello everyone!");
+            vasya.Jump();
+        }
+    }    
+
+
+    interface IHuman {
+            void Jump();
+            void Speak(string message);
+        }
+
+
+    class User : IHuman {
+
+        string Name;
+        public User(string name){
+            Name = name;
+        }
+
+        public void Jump(){
+            Console.WriteLine(this.Name + " подпрыгнул!");
+        }
+
+        public void Speak(string msg){
+            Console.WriteLine($"{this.Name} говорит: \"{msg}\"");
+        }
+    }
 }
-
-
-
-class User : IHuman {
-
-    string Name;
-    public User(string name){
-        Name = name;
-    }
-
-    public void Jump(){
-        Console.WriteLine(this.Name + " подпрыгнул!");
-    }
-
-    public void Speak(string msg){
-        Console.WriteLine($"{this.Name} говорит: \"{msg}\"");
-    }
-}
-
