b37ff5b39d789da69b95e2cb826a41de7d016fb8
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-12-21 16:44:30 +0300

    Update Program.cs
    iterate over 2d array

--- Program.cs
+++ Program.cs
@@ -10,21 +10,21 @@ namespace C_Sharp_Basics {
         static void Main(string[] args) {
             
 
-            int[] myArr = new int[10];
-            Random rnd = new Random();
-            int summ =0;
-
-            for (int i = 0; i < myArr.Length; i++)
+            int[,] arr2d = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}
+            };
+        
+            for (int i = 0; i <= arr2d.GetUpperBound(0); i++)
             {
-                myArr[i] = rnd.Next(-10,11);
-                System.Console.WriteLine(myArr[i]);
-                
-                summ += myArr[i];
+                for (int j = 0; j <= arr2d.GetUpperBound(1); j++)
+                {
+                    System.Console.WriteLine(arr2d[i,j]);
+                }
             }
+        
 
-            System.Console.WriteLine("SUMM = " + summ);
-        
-        
         }
     }    
 } 
