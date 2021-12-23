d01f7ecabcb31d2bb2f1f98097c13858f7c8d8d2
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-12-23 10:50:22 +0300

    class example

--- Program.cs
+++ Program.cs
@@ -7,12 +7,17 @@ namespace C_Sharp_Basics {
     // класс - по имени файла
     class Program {
 
+
         // метод Main() - единственный на проект, точка входа в исполнение программы
         static void Main(string[] args) {
-            Print();
-            Print("Hello everybody!");
-            Print(Summ(12.5, 0.76).ToString());
+            
+            Robot bot = new Robot();
+            bot.name = "Robo";
+            bot.weight = 800;
+            bot.coords = new int[] {0, 0, 0};
+
         }
+
 
         // public - метод доступен по всему проекту
         // static - не требуется создавать экземпляр класса для вызова метода, метод принадлежит самому классу
@@ -20,6 +25,7 @@ namespace C_Sharp_Basics {
         public static void Print(string msg = "") {
             System.Console.WriteLine(msg);
         }
+
 
         public static double Summ(double a, double b) {
             double result = a + b;
---
+++ Robot.cs
@@ -0,0 +1,11 @@
+namespace C_Sharp_Basics {
+
+
+    class Robot {
+
+        public string name;
+        public int weight;
+        public int[] coords;
+
+    }
+}
