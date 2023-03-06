b042e4942ffc9529d8f7f5729f6e1dae3d71b524
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2023-03-06 12:19:29 +0300

    refactor: project structure

--- Program.cs
+++ Program.cs
@@ -16,13 +16,10 @@ namespace C_Sharp_Basics {
             // Example.OOP();
             // Example.LotteryStat(1000);
 
-
             DateTime start1 = new DateTime(2022,12,28,9,30,0); // 28.12.2022 9:30
             DateTime end1 = new DateTime(2022,12,28,12,30,0); // 28.12.2022 12:30
-
             DateTime start2 = new DateTime(2022,12,28,13,0,0); // 28.12.2022 13:00
             DateTime end2 = new DateTime(2022,12,28,15,0,0); // 28.12.2022 15:00
-
             Example.GetTimeIntersection(start1, end1, start2, end2);
 
         }
