91d336c9b7282be8b84c8538744f5367448f3547
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2024-05-13 09:20:02 +0300

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -20,7 +20,7 @@ namespace C_Sharp_Basics {
             DateTime end1 = new DateTime(2022,12,28,12,30,0); // 28.12.2022 12:30
             DateTime start2 = new DateTime(2022,12,28,13,0,0); // 28.12.2022 13:00
             DateTime end2 = new DateTime(2022,12,28,15,0,0); // 28.12.2022 15:00
-            Example.GetTimeIntersection(start1, end1, start2, end2);
+            // Example.GetTimeIntersection(start1, end1, start2, end2);
 
         }
     }
